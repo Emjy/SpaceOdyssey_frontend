d5a195d11ced2fd414c928cd3313081ee63e5920
Emilien <emiliengir@gmail.com>
2024-04-19 13:41:41 +0200

    correction infos vercel

--- functions/utils.js
+++ functions/utils.js
@@ -1,6 +1,6 @@
 // Fonction pour récupération des informations sur l'objet en cours
 export const infoObjet = async (objectName, setInfos) => {
-    const response = await fetch(`http://localhost:3000/bodies/body/${objectName}`);
+    const response = await fetch(`https://space-odyssey-backend.vercel.app/bodies/body/${objectName}`);
     const data = await response.json();
     if (data.result) {
         setInfos(data.body);
